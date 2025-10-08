b61a1e6e447d8ae57b969f2c0ffe89df4474052e
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2025-10-08 23:16:15 +0530

    working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,40 @@
 import React from 'react'
-import Navbar from '../../layouts/Navbar'
+import Image from 'next/image';
 
 const Page = () => {
   return (
     <div className='bg-[#373737] min-h-screen p-5 space-y-5 relative'>
-      <Navbar />
+      <NavBar />
 
       <HeroSection />
     </div>
   )
 };
 
+const NavBar = () => {
+  return (
+    <div className="bg-[#EBE9E9] p-4 flex flex-row justify-between items-center rounded-2xl sticky top-0 w-full">
+      <div className="relative flex-shrink-0 h-14 w-48">
+        <Image
+          src={"/logo.svg"}
+          alt='Super CX'
+          fill
+          className='object-contain pointer-events-none select-none'
+        />
+      </div>
+
+      <button className='custom-button px-6 py-3 rounded-full'>
+        Get Started
+      </button>
+    </div>
+  )
+}
+
 const HeroSection = () => {
   return (
-    <div className="p-4 rounded-2xl bg-[#EBE9E9] w-full">
-      <div className="p-16 pr-0 w-1/2 space-y-8">
-        <h1 className='text-6xl italic font-extrabold leading-normal uppercase' style={{letterSpacing: "0.05em"}}>
+    <div className="p-4 rounded-2xl bg-[#EBE9E9] w-full flex flex-row items-start">
+      <div className="p-16 pr-0 flex-1 space-y-8">
+        <h1 className='text-6xl italic font-extrabold leading-normal uppercase' style={{ letterSpacing: "0.05em" }}>
           Drive Revenue Growth with Data-Backed Customer
           <img src={"/spark.svg"} alt='' className='h-[90px] w-[90px] inline mx-8' />
           Engagement
@@ -25,6 +44,8 @@ const HeroSection = () => {
           SuperCX powers smarter, faster growth with chatbots, CRM, and marketing automation.
         </p>
       </div>
+
+      <div className="flex-1"></div>
     </div>
   )
 }
