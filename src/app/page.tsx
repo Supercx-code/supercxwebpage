7227857cd467936a5cb52d9411c253a648091d0c
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2025-10-09 10:15:10 +0530

    working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
+import HeadinText from '../../components/HeadinText';
 
 const Page = () => {
   return (
@@ -13,7 +14,7 @@ const Page = () => {
 
 const NavBar = () => {
   return (
-    <div className="bg-[#EBE9E9] p-4 flex flex-row justify-between items-center rounded-2xl sticky top-0 w-full">
+    <div className="bg-[#EBE9E9] p-4 flex flex-row justify-between items-center rounded-2xl sticky top-0 w-full shadow">
       <div className="relative flex-shrink-0 h-14 w-48">
         <Image
           src={"/logo.svg"}
@@ -23,7 +24,7 @@ const NavBar = () => {
         />
       </div>
 
-      <button className='custom-button px-6 py-3 rounded-full'>
+      <button className='custom-yellow-button px-6 py-3 rounded-full'>
         Get Started
       </button>
     </div>
@@ -34,15 +35,26 @@ const HeroSection = () => {
   return (
     <div className="p-4 rounded-2xl bg-[#EBE9E9] w-full flex flex-row items-start">
       <div className="p-16 pr-0 flex-1 space-y-8">
-        <h1 className='text-6xl italic font-extrabold leading-normal uppercase' style={{ letterSpacing: "0.05em" }}>
-          Drive Revenue Growth with Data-Backed Customer
-          <img src={"/spark.svg"} alt='' className='h-[90px] w-[90px] inline mx-8' />
-          Engagement
-        </h1>
+        <HeadinText>
+          <>
+            Drive Revenue Growth with Data-Backed Customer
+            <img src={"/spark.svg"} alt='' className='h-[90px] w-[90px] inline mx-8 pointer-events-none select-none' />
+            Engagement
+          </>
+        </HeadinText>
 
         <p className='text-2xl font-light'>
           SuperCX powers smarter, faster growth with chatbots, CRM, and marketing automation.
         </p>
+
+        <div className="flex flex-row gap-4">
+          <button className='custom-yellow-button px-8 py-3 rounded-full'>
+            Book A Free Consultation
+          </button>
+          <button className='custom-black-button px-8 py-3 rounded-full'>
+            Explore Our Products
+          </button>
+        </div>
       </div>
 
       <div className="flex-1"></div>
