a4053a7db876ce1c4ed6a82c68d96f267ba449f4
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2025-10-10 13:06:13 +0530

    90% done

--- components/InfiniteMoving.tsx
+++ components/InfiniteMoving.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const InfiniteMoving = () => {
     return (
-        <div className='animate-to-left flex flex-row overflow-x-hidden w-full'>
+        <div className='animate-to-left flex flex-row gap-10 overflow-x-hidden w-full'>
             <img
                 alt='Companies'
                 className='h-full w-full'
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import React, { JSX } from 'react'
 import Image from 'next/image';
 import InfiniteMoving from '../../components/InfiniteMoving';
+import { IoStarSharp } from 'react-icons/io5';
+import { RiArrowRightLine } from 'react-icons/ri';
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+import Link from 'next/link';
 
 const Page = () => {
   return (
@@ -16,6 +21,16 @@ const Page = () => {
       <OurProducts />
 
       <ServicesCounts />
+
+      <Highlight />
+
+      <Reviews />
+
+      <Solutions />
+
+      <ReadyToScale />
+
+      <Footer />
     </div>
   )
 };
@@ -172,7 +187,7 @@ const HowWeWork = () => {
   return (
     <div className='flex flex-row gap-4 w-full'>
       {list.map(({ image, title, description }) => (
-        <div className='bg-white flex-1 p-6 flex flex-col items-center text-center gap-4 rounded-2xl'>
+        <div key={title} className='bg-white flex-1 p-6 flex flex-col items-center text-center gap-4 rounded-2xl'>
           <div className="relative w-14 h-14">
             <Image
               src={image}
@@ -260,7 +275,7 @@ const ServicesCounts = () => {
   return (
     <div className='flex flex-row gap-4 w-full'>
       {list.map(({ number, sign, description }) => (
-        <div className='bg-white flex-1 p-6 pt-10 flex flex-col items-center text-center gap-4 rounded-2xl'>
+        <div key={number} className='bg-white flex-1 p-6 pt-10 flex flex-col items-center text-center gap-4 rounded-2xl'>
           <h4 className='text-5xl font-[500]'>
             {number}{sign}
           </h4>
@@ -268,6 +283,263 @@ const ServicesCounts = () => {
           <p className='text-base font-normal text-center'>{description}</p>
         </div>
       ))}
+    </div>
+  )
+};
+
+const Highlight = () => {
+  return (
+    <div className="p-16 bg-[#B66CFF] rounded-2xl">
+
+    </div>
+  )
+};
+
+const Reviews = () => {
+  const reviewsList = [
+    {
+      companyImage: "/reviews/360.svg",
+      company: "Rewards 360",
+      review: "“SuperCX transformed our customer support — response times cut from 12 hours to 1.5 hours+”",
+      stars: 5,
+      reviewBy: "CX HEAD",
+      department: "Retail Brand"
+    },
+    {
+      companyImage: "/reviews/marhabba.svg",
+      company: "Rewards 360",
+      review: "“SuperCX transformed our customer support — response times cut from 12 hours to 1.5 hours+”",
+      stars: 5,
+      reviewBy: "CX HEAD",
+      department: "Retail Brand"
+    },
+  ];
+
+  return (
+    <div className='flex flex-row w-full gap-4'>
+      {reviewsList.map(({ companyImage, company, review, stars, reviewBy, department }, index) => (
+        <div key={index} className="w-full bg-white p-6 rounded-2xl space-y-8">
+          <div className="flex flex-row items-start justify-between">
+            <img src={companyImage} alt={company} className='w-fit h-fit' />
+
+            <div className="relative w-10 h-10 bg-black rounded-full">
+              <Image
+                src={"/reviews/qoute.svg"}
+                alt={"”"}
+                fill
+                className='p-2'
+              />
+            </div>
+          </div>
+
+          <p className='text-4xl italic font-bold leading-normal uppercase'>{review}</p>
+
+          <div className="flex flex-row items-center justify-between">
+            <p className='text-lg'>
+              <span className='font-semibold'>
+                {reviewBy},{" "}
+              </span>
+              {department}
+            </p>
+
+            <Stars rating={stars} />
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const Solutions = () => {
+  const list = [
+    {
+      image: "/solutions/commerce.svg",
+      title: "E-commerce",
+      description: "Abandoned cart recovery, WhatsApp bots"
+    },
+    {
+      image: "/solutions/bfsi.svg",
+      title: "BFSI",
+      description: "Secure, multilingual chatbots, voice banking."
+    },
+    {
+      image: "/solutions/healthcare.svg",
+      title: "HEalthcare",
+      description: "Appointment scheduling, telehealth bots."
+    },
+    {
+      image: "/solutions/saas.svg",
+      title: "Saas",
+      description: "Lead nurturing automation, helpdesk bots."
+    },
+  ];
+
+  return (
+    <div className='bg-[#FF9D6C] p-16 rounded-2xl'>
+      <HeadingText>
+        <>
+          Solutions by industry
+          <img src="/sparks.svg" alt="" className='mx-8 inline pointer-events-none select-none' />
+        </>
+      </HeadingText>
+
+      <div className='flex flex-row gap-4 w-full mt-10'>
+        {list.map(({ image, title, description }) => (
+          <div key={title} className='bg-white flex-1 p-6 flex flex-col items-center text-center gap-4 rounded-2xl'>
+            <div className="relative w-14 h-14">
+              <Image
+                src={image}
+                alt={title}
+                fill
+              />
+            </div>
+
+            <h3 className='text-xl uppercase font-medium text-center'>{title}</h3>
+
+            <p className='text-base font-normal text-center'>{description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+};
+
+const ReadyToScale = () => {
+  return (
+    <div className="bg-[#EBE9E9] p-16 rounded-2xl text-center flex flex-col gap-10 items-center">
+      <div className="max-w-3xl space-y-4">
+        <HeadingText>
+          <>
+            Ready to Scale Smarter with AI?
+          </>
+        </HeadingText>
+
+        <p className='text-2xl font-normal'>
+          “Let’s transform your customer experience with SuperCX.”
+        </p>
+      </div>
+
+      <button className='text-white custom-black-button mt-auto text-xl font-medium w-fit rounded-full py-3 px-12'>
+        Let's Get Started
+      </button>
+    </div>
+  )
+};
+
+const Footer = () => {
+  const Links = [
+    {
+      heading: "Company",
+      links: [
+        { title: "About Us", link: "#", },
+        { title: "Careers", link: "#" },
+        { title: "Contact", link: "#" },
+      ]
+    },
+    {
+      heading: "Products",
+      links: [
+        { title: "SuperCX", link: "#", },
+        { title: "SuperVoice", link: "#" },
+        { title: "SuperDesk", link: "#" },
+      ]
+    },
+    {
+      heading: "Services",
+      links: [
+        { title: "AI Chatbots", link: "#", },
+        { title: "CRM Implementation", link: "#" },
+        { title: "Helpdesk Support", link: "#" },
+        { title: "Marketing Automation", link: "#" },
+        { title: "BPM Consulting", link: "#" },
+        { title: "Integration Services", link: "#" },
+      ]
+    },
+    {
+      heading: "Services",
+      links: [
+        { title: "AI Chatbots", link: "#", },
+        { title: "CRM Implementation", link: "#" },
+        { title: "Helpdesk Support", link: "#" },
+        { title: "Marketing Automation", link: "#" },
+        { title: "BPM Consulting", link: "#" },
+        { title: "Integration Services", link: "#" },
+      ]
+    },
+    {
+      heading: "Solutions",
+      links: [
+        { title: "E-commerce", link: "#", },
+        { title: "BFSI", link: "#" },
+        { title: "Healthcare", link: "#" },
+        { title: "Marketing Automation", link: "#" },
+        { title: "SaaS", link: "#" },
+      ]
+    },
+  ]
+
+  return (
+    <div className="p-16 bg-[#EBE9E9] rounded-2xl space-y-16">
+      <div className="w-full bg-[#69EEC6] rounded-2xl p-8 shadow-[-0.4em_0.4em_#000] flex flex-row justify-between">
+        <p className='text-4xl italic font-extrabold leading-normal uppercase'>
+          Subscribe Now!
+        </p>
+
+        <div className="border-2 border-black flex flex-row items-center rounded-xl relative w-1/3">
+          <input type="email"
+            name="email"
+            id="email"
+            className='p-3 bg-transparent placeholder-black focus:outline-none focus:bg-transparent w-full h-full pr-10 rounded-xl'
+            placeholder='Your Email Address'
+          />
+
+          <button className='cursor-pointer absolute right-0.5 hover:bg-[#69EEC6] px-4 h-[90%] rounded-xl'>
+            <RiArrowRightLine className='text-xl' />
+          </button>
+        </div>
+      </div>
+
+      <div className="w-full flex xl:flex-row flex-col gap-x-16 gap-y-4 flex-wrap items-start justify-between">
+        <div className="relative flex-shrink-0 h-14 w-48">
+          <Image
+            src={"/logo.svg"}
+            alt='Super CX'
+            fill
+            className='object-contain pointer-events-none select-none'
+          />
+        </div>
+
+        <div className="lg:flex grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 flex-1 w-full flex-row flex-wrap items-start justify-between gap-6">
+          {Links.map(({ heading, links }, index) => (
+            <div key={index} className="flex flex-col space-y-1">
+              <h3 className='font-medium mb-2 text-lg'>{heading}</h3>
+
+              {links.map(({ title, link }) => (
+                <Link key={title} href={link}>
+                  {title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='border-t-2 border-black pt-6 flex flex-row items-center justify-between'>
+        <div className="flex flex-row items-center gap-4 text-xl">
+          <FaFacebookF />
+          <FaXTwitter />
+          <FaInstagram />
+          <FaLinkedinIn />
+        </div>
+
+        <span className='text-sm font-medium'>
+          &copy; 2025 SuperCX. All rights reserved.
+        </span>
+
+        <span className='text-sm font-medium'>
+          Privacy & Cookie Poilcy
+        </span>
+      </div>
     </div>
   )
 }
@@ -327,11 +599,29 @@ const ProductsCard = ({ image, title, description }: { image: string; title: str
 
       <p className='text-base font-normal text-center'>{description}</p>
 
-      <button className='text-white mt-auto text-xl font-medium bg-black w-full rounded-md p-2'>
+      <button className='text-white custom-black-button mt-auto text-xl font-medium w-full rounded-md p-2'>
         Learn More
       </button>
     </div>
   )
-}
+};
+
+const Stars = ({
+  rating,
+}: {
+  rating: number;
+}) => {
+  return (
+    <div className="flex gap-2">
+      {Array.from({ length: 5 }, (_, i) => (
+        <IoStarSharp
+          key={i}
+          className={`sm:h-6 h-4 sm:w-6 w-4 ${i < rating ? "text-yellow-400" : "text-gray-300"
+            }`}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Page;
